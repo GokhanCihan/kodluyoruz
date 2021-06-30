225b93d17f62e4415a331eb36528577f83db64c2
Gokhan Cihan <cihangokhangmail.com>
2021-06-30 09:35:03 +0300

    layout changed

--- myscript.js
+++ myscript.js
@@ -11,24 +11,21 @@ function myDateFunction() {
     let [month, mDay, wDay] = [date.getMonth(), date.getDate(), date.getDay()];
     let [hours, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()]
     
-    let reachDayOfMonth = document.getElementById('gunSayi')
-    let reachMonth = document.getElementById('ay')
     let reachClock = document.getElementById('saat')
-    let reachDayOfWeek = document.getElementById('gunIsim')
-    document.getElementById('place').innerHTML = `Saat:`
+    let reachDate = document.getElementById('tarih')
+    
     if (mDay<10) (mDay = `0${mDay}`) 
-    reachDayOfMonth.innerHTML = `${mDay}`
+   
     
     let months = ['Ocak','Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık']
-    reachMonth.innerHTML = `${months[month]}`;
     
     let days = ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi']
-    reachDayOfWeek.innerHTML = `${days[wDay]}`
+    reachDate.innerHTML = `${mDay} ${months[month]} ${days[wDay]}`
     
     if (hours<10) (hours = `0${hours}`)
     if (minutes<10) (minutes = `0${minutes}`)
     if (seconds<10) (seconds = `0${seconds}`)
-    reachClock.innerHTML = `${hours}:${minutes}:${seconds}`
+    reachClock.innerHTML = `Saat: ${hours}:${minutes}:${seconds}`
 }
 
 askUserName();
