0e5feb7aef7f53daf157444ad5f335c9aa55cfbd
Gokhan Cihan <cihangokhangmail.com>
2021-07-03 17:41:13 +0300

    comments

--- javascript-odev2/js/script.js
+++ javascript-odev2/js/script.js
@@ -1,34 +1,39 @@
 
-/* 
-1. Create a node for li element
-2. Get text inside of the input field(id=task) with value property
-3. (if true)Create a node and assign the text value to this node
-4. Append text node to list node
-5. Append list node to the list  
-*/
+
 
 var ulList = document.getElementById("list")
 function newElement(){
+    /* 
+    1. Create a node for li element
+    2. Get text inside of the input field(id=task) with value property
+    3. (if true)Create a node and assign the text value to this node
+    4. Append text node to list node
+    5. Append list node to the list 
+    */
     var newListNode = document.createElement("LI");
     var newButtonNode = document.createElement("BUTTON")
     var valueNode = document.getElementById("task").value;
   
     if(valueNode){
 
-        /*new li element*/
+        /* add properties for li element */
         var textNode = document.createTextNode(valueNode);
         newListNode.appendChild(textNode);
         newListNode.setAttribute("onclick","clickedList(this)")
-        ulList.appendChild(newListNode);
-
-        /*new button element (child of li)*/
+        
+        /* add properties for button */
         newButtonNode.classList.add("button-remove")
         newButtonNode.setAttribute("type","button")
         newButtonNode.setAttribute("onclick","removesParentLi(this)")
         newButtonNode.innerHTML = `&times;`
-        ulList.lastChild.appendChild(newButtonNode);
         
-        /*raise toast*/
+        /* append button to the li element */ 
+        newListNode.appendChild(newButtonNode)
+        
+        /* append full li node to the parent ul element */
+        ulList.appendChild(newListNode);
+      
+        /* raise toast */
         $(document).ready(function(){
             $('.success').toast('show');
           });
@@ -38,7 +43,6 @@ function newElement(){
         });
     }
 }
-
 
 function clickedList(element){
     if(element.getAttribute("class")){
@@ -51,3 +55,4 @@ function clickedList(element){
 function removesParentLi(element){
     element.parentNode.remove()
 }
+
