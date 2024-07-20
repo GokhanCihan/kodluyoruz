33d1b53dd294d89cfb8b7517544eced5cc498678
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-07-20 16:38:23 +0300

    save and retrieve tasks from localStorage

--- javascript-odev2/js/script.js
+++ javascript-odev2/js/script.js
@@ -1,8 +1,44 @@
-function createTask() {
-    const task = document.getElementById("task").value;
+let tasks;
+const defaultTasks = [ 
+    { 
+        "task": "Yemek yap",
+        "completed": false,
+    }, 
+    { 
+        "task": "50 sayfa kitap oku",
+        "completed": false,
+    },
+    { 
+        "task": "3 litre su iç",
+        "completed": false,
+    },
+];
+loadTasks();
+
+function loadTasks() {
+    if(!localStorage.getItem('tasks')) {
+        localStorage.setItem('tasks', JSON.stringify(defaultTasks));
+    }
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.forEach(element => {
+        let item = ListItem(element.task);
+        if (element.completed) {
+            toggleCheck(item);
+        }
+        document.getElementById("list").appendChild(item);
+    })
+}
+
+function addTask() {
+    const value = document.getElementById("task").value;
   
-    if(task) {
-        document.getElementById("list").appendChild(ListItem(task));
+    if(value && !(value === " ")) {
+        document.getElementById("list").appendChild(ListItem(value));
+
+        // update localStorage
+        tasks.push({ "task": value, "completed": false});
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
         $(document).ready(function(){
             $('.success').toast('show');
         });
@@ -13,6 +49,7 @@ function createTask() {
     }
 }
 
+// create node for <li> element
 function ListItem(task) {
     const listItem = document.createElement("li");
     listItem.appendChild(document.createTextNode(task));
@@ -22,25 +59,49 @@ function ListItem(task) {
     return listItem;
 }
 
+// create node for <button> element
 function Button() {
     const button = document.createElement("button");
     button.classList.add("button-remove");
     button.setAttribute("type", "button");
-    button.setAttribute("onclick", "removeParent(this)");
-    button.innerHTML = `&times;`;
+    button.setAttribute("onclick", "removeTask(this)");
+    button.innerHTML = "X";
 
     return button;
 }
 
 function toggleCheck(element) {
+    // remove button text
+    let toggledTask = element.innerText.replace("X", "");
+    
+    //find and modify toggled task in the array before saving
     if(element.getAttribute("class")){
-        element.removeAttribute("class")
-    }else{
-        element.setAttribute("class", "checked")
+        element.removeAttribute("class");
+        tasks.forEach(arrayItem => {
+            if(arrayItem.task === toggledTask){
+                arrayItem.completed = false;
+            };
+        })
+    }else {
+        element.setAttribute("class", "checked");
+        tasks.forEach(arrayItem => {
+            if(arrayItem.task === toggledTask){
+                arrayItem.completed = true;
+            };
+        })
     }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function removeParent(element) {
+function removeTask(element) {
+    let removedTask = element.parentNode.innerText.replace("X", "");
+    let index;
+    tasks.forEach(arrayItem => {
+        if(arrayItem.task === removedTask){
+            index = tasks.indexOf(arrayItem);
+        };
+    })
+    tasks.splice(index, 1);
     element.parentNode.remove()
 }
 
