35702911797a50da0f4bbf3a9968345318ea2590
Gökhan Cihan <83141061+GokhanCihan@users.noreply.github.com>
2024-07-20 11:32:30 +0300

    refactor

--- javascript-odev2/js/script.js
+++ javascript-odev2/js/script.js
@@ -1,54 +1,46 @@
-const list = document.getElementById("list")
-function newElement(){
-    /* 
-    1. Create a node for li element
-    2. Get text inside of the input field(id=task) with value property
-    3. (if true)Create a node and assign the text value to this node
-    4. Append text node to list node
-    5. Append list node to the list 
-    */
-    const listItem = document.createElement("li");
-    const buttonRemove = document.createElement("button")
+function createTask() {
     const task = document.getElementById("task").value;
   
-    if(task){
-        /* add properties for li element */
-        const textNode = document.createTextNode(task);
-        listItem.appendChild(textNode);
-        listItem.setAttribute("onclick", "getListItem(this)")
-        
-        /* add properties for button */
-        buttonRemove.classList.add("button-remove")
-        buttonRemove.setAttribute("type", "button")
-        buttonRemove.setAttribute("onclick", "removeParent(this)")
-        buttonRemove.innerHTML = `&times;`
-        
-        /* append button to the li element */ 
-        listItem.appendChild(buttonRemove)
-        
-        /* append full li node to the parent ul element */
-        list.appendChild(listItem);
-      
-        /* raise toast */
+    if(task) {
+        document.getElementById("list").appendChild(ListItem(task));
         $(document).ready(function(){
             $('.success').toast('show');
-          });
-    }else{
+        });
+    }else {
         $(document).ready(function(){
             $('.error').toast('show');
         });
     }
 }
 
-function getListItem(element){
+function ListItem(task) {
+    const listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(task));
+    listItem.setAttribute("onclick", "toggleCheck(this)")
+    listItem.appendChild(Button());
+
+    return listItem;
+}
+
+function Button() {
+    const button = document.createElement("button");
+    button.classList.add("button-remove");
+    button.setAttribute("type", "button");
+    button.setAttribute("onclick", "removeParent(this)");
+    button.innerHTML = `&times;`;
+
+    return button;
+}
+
+function toggleCheck(element) {
     if(element.getAttribute("class")){
         element.removeAttribute("class")
     }else{
-        element.setAttribute("class","checked")
+        element.setAttribute("class", "checked")
     }
 }
 
-function removeParent(element){
+function removeParent(element) {
     element.parentNode.remove()
 }
 
